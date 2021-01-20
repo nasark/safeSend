5122832a2a6b845b5ad5c8658c9874ce7514ff4e
Nasar Khan <nasarak_786@hotmail.com>
2021-01-19 22:15:38 -0500

    added access history list

--- Access.js
+++ Access.js
@@ -1,20 +1,81 @@
+/* eslint-disable react/self-closing-comp */
 /* eslint-disable react-native/no-inline-styles */
 /* eslint-disable prettier/prettier */
 import React from 'react';
-import { View, Text } from 'react-native';
+import { Dimensions, StyleSheet, ScrollView } from 'react-native';
+const { Block, Card } = require('galio-framework');
+
+const { width } = Dimensions.get('screen');
+
+const cards = [
+  {
+    id: 1,
+    title: 'package-name1',
+    caption: 'Last updated:\n15 minutes ago',
+    location: 'On the way',
+    status: true,
+  },
+  {
+    id: 2,
+    title: 'package-name2',
+    caption: 'Last updated:\n2d ago',
+    location: 'Delivered',
+    status: true,
+  },
+  {
+    id: 3,
+    title: 'package-name3',
+    caption: 'Last updated:\n7d ago\nTap for more details',
+    location: 'Package missing',
+    status: false,
+  },
+  {
+    id: 4,
+    title: 'poggers',
+    caption: 'Last updated:\n9d ago',
+    location: 'idk',
+    status: false,
+  },
+];
 
 class Access extends React.Component {
-  constructor(props) {
-    super(props);
-  }
-
   render() {
     return (
-        <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
-            <Text>Access Screen</Text>
-        </View>
+      <ScrollView contentContainerStyle={styles.cards} backgroundColor="white">
+        <Block flex space="between">
+          {cards && cards.map((card, id) => (
+            <Card
+              key={`card-${id}`}
+              flex
+              borderless
+              shadowColor={'black'}
+              style={styles.card}
+              title={card.title}
+              caption={card.caption}
+              location={card.location}
+              locationColor={card.status ? 'green' : 'red'}
+            >
+            </Card>
+          ))}
+        </Block>
+      </ScrollView>
     );
   }
-
 }
+
+const styles = StyleSheet.create({
+  cards: {
+    width,
+    backgroundColor: 'white',
+    alignItems: 'center',
+    justifyContent: 'flex-start',
+  },
+  card: {
+    backgroundColor: 'white',
+    width: width - 16 * 2,
+    marginVertical: 16 * 0.875,
+    elevation: 16 / 2,
+  },
+});
+
 export default Access;
